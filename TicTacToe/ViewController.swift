531917150d5fba4ee839db48becba09c61c16e45
Jakub Tomkiewicz <kubatomki@gmail.com>
2020-04-14 14:05:08 +0200

    Initial commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by J Tomkiewicz on 14/04/2020.
+//  Copyright © 2020 JTomkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var score: UILabel!
+    
+    @IBAction func action(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
